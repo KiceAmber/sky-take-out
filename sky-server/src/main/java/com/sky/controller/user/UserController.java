da73e60226fc80d2a1791c72b4e9b2051764c937
KiceAmber <kiceamber@gmail.com>
2023-10-14 15:39:16 +0800

    feat(order): 实现用户微信支付功能，但是又没有完全实现

---
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -0,0 +1,12 @@
+package com.sky.controller.user;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/user")
+@Api(tags = "C端-用户接口")
+public class UserController {
+
+}
